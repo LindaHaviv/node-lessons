abf904f363c8c34238672f5ccb340a4a6ec62737
LindaHaviv <linda.haviv@gmail.com>
2017-08-06 23:21:26 -0400

    added note app fucntionality

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,19 +1,61 @@
 console.log('Starting notes.js')
 
-var addNote = (title, body) => {
-	console.log('Adding note', title, body);
+const fs = require('fs');
+
+var fetchNotes = () => {
+   try {
+    var notesString = fs.readFileSync('notes-date.json')
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+
+  }
+
 };
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-date.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+	var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};  
 
 var getAll = () => {
 	console.log('Getting all notes');
 };
 
 var getNote = (title) => {
-	console.log('Getting note', title);
+	var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 };
 
 var removeNote = (title) => {
-	console.log('removing note', title);
+	var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+  
+  return notes.length !== filteredNotes.length;
+  
+};
+
+var logNote = (note) => {
+  console.log('--');
+  console.log(`Title: ${note.title}`)
+  console.log(`Body: ${note.body}`)
 };
 
 module.exports = {
@@ -21,6 +63,7 @@ module.exports = {
 	addNote,
 	getAll,
 	getNote,
-	removeNote
+	removeNote,
+  logNote
 };
 
